16e073c7c88a7690910518e62a16772b2e369206
mabushanab <m.m.abushanab94@gmail.com>
2021-07-13 00:20:01 +0300

    Adding TC-005

---
+++ Object Repository/PrivacyPage/link_legal.rs
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>link_legal</name>
+   <tag></tag>
+   <elementGuidId>79347312-5e94-447d-ae6b-473b777754a9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@href=&quot;http://www.fetchr.us/legal&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
