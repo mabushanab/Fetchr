ddfc6d6c247053dc4c573c52a1b772eacb717bee
mabushanab <m.m.abushanab94@gmail.com>
2021-07-13 01:38:25 +0300

    preparing TCs(006-017),implementing TCs(006,007,008,009,016 and 017)

---
+++ Object Repository/HomePage/label-CurrentStatusEn.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>label-CurrentStatusEn</name>
+   <tag></tag>
+   <elementGuidId>7a93e8e1-5028-4521-a86b-e8aed7f788b4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[contains(text(),&quot;Current Status&quot;)]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
