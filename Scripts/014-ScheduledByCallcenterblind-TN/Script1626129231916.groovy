1dfb4679cfd6965c79d89177650f82fe9c0ac79f
mabushanab <m.m.abushanab94@gmail.com>
2021-07-18 18:13:14 +0300

    Finalizing the project

--- Scripts/014-ScheduledByCallcenterblind-TN/Script1626129231916.groovy
+++ Scripts/014-ScheduledByCallcenterblind-TN/Script1626129231916.groovy
@@ -1,20 +1,20 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import com.kms.katalon.core.testcase.TestCase as TestCase
 import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
 import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import org.openqa.selenium.Keys as Keys
 
 WebUI.openBrowser('')
@@ -29,75 +29,127 @@ WebUI.waitForElementPresent(findTestObject('HomePage/label_Covid-19-ResourcesUpd
 
 WebUI.verifyElementVisible(findTestObject('HomePage/label_Covid-19-ResourcesUpdates'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.waitForElementClickable(findTestObject('HomePage/link_PrivacyPolicy'), 30)
+WebUI.waitForElementClickable(findTestObject('HomePage/link_EndCovid'), 30)
 
-WebUI.click(findTestObject('HomePage/link_PrivacyPolicy'), FailureHandling.STOP_ON_FAILURE)
+WebUI.click(findTestObject('HomePage/link_EndCovid'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.waitForElementClickable(findTestObject('PrivacyPage/label_FtchrPrivacyPolicy'), 30)
+WebUI.setText(findTestObject('HomePage/textBox_Search'), GlobalVariable.TN5)
 
-WebUI.verifyElementVisible(findTestObject('PrivacyPage/label_FtchrPrivacyPolicy'), FailureHandling.STOP_ON_FAILURE)
+WebUI.waitForElementClickable(findTestObject('HomePage/btn_Search'), 30)
 
-WebUI.verifyElementVisible(findTestObject('TandC/img_FetchrLogoEn'), FailureHandling.STOP_ON_FAILURE)
+WebUI.click(findTestObject('HomePage/btn_Search'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.verifyElementVisible(findTestObject('TandC/icon_CallUs'), FailureHandling.STOP_ON_FAILURE)
+WebUI.waitForElementPresent(findTestObject('HomePage/label_CurrentStatusEn'), 30)
 
-WebUI.verifyElementVisible(findTestObject('TandC/link_ChangeLangAr'), FailureHandling.STOP_ON_FAILURE)
+WebUI.verifyElementVisible(findTestObject('HomePage/label_CurrentStatusEn'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.verifyElementVisible(findTestObject('PrivacyPage/link_legal'), FailureHandling.STOP_ON_FAILURE)
+WebUI.verifyElementVisible(findTestObject('HomePage/label_CurrentStatusDescEn'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.verifyElementText(findTestObject('PrivacyPage/link_legal'), 'www.fetchr.us/legal')
+String temp = WebUI.getText(findTestObject('HomePage/label_CurrentStatusDescEn'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.verifyElementAttributeValue(findTestObject('PrivacyPage/link_legal'), 'href', 'http://www.fetchr.us/legal', 30)
-
-WebUI.verifyElementVisible(findTestObject('PrivacyPage/link_ads-choices'), FailureHandling.STOP_ON_FAILURE)
-
-WebUI.verifyElementText(findTestObject('PrivacyPage/link_ads-choices'), 'www.aboutads.info/choices')
-
-WebUI.verifyElementAttributeValue(findTestObject('PrivacyPage/link_ads-choices'), 'href', 'http://www.aboutads.info/choices', 
-    30)
-
-WebUI.verifyElementVisible(findTestObject('PrivacyPage/link_fetche-support'), FailureHandling.STOP_ON_FAILURE)
-
-WebUI.verifyElementText(findTestObject('PrivacyPage/link_fetche-support'), 'support@fetchr.us')
-
-WebUI.verifyElementAttributeValue(findTestObject('PrivacyPage/link_fetche-support'), 'href', 'mailto:support@fetchr.us', 
-    30)
-
-temp = WebUI.getUrl()
-
-if (!(temp.equals('http://track.fetchr.us/privacy'))) {
+if (!(temp.contains(GlobalVariable.ScheduledByCallcenterblind))) {
     assert false
 }
 
-WebUI.verifyElementAttributeValue(findTestObject('TandC/label_CallUs'), 'href', 'tel:+97148018100', 30)
+WebUI.verifyElementVisible(findTestObject('HomePage/btn_OrderHistory'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.waitForElementClickable(findTestObject('TandC/link_ChangeLangAr'), 30, FailureHandling.CONTINUE_ON_FAILURE)
+WebUI.verifyElementVisible(findTestObject('HomePage/btn_RescheduleOrder'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.click(findTestObject('TandC/link_ChangeLangAr'), FailureHandling.CONTINUE_ON_FAILURE)
+WebUI.waitForElementClickable(findTestObject('HomePage/btn_OrderHistory'), 30)
 
-WebUI.waitForElementPresent(findTestObject('TandC/link_ChangeLangEn'), 10, FailureHandling.CONTINUE_ON_FAILURE)
+WebUI.click(findTestObject('HomePage/btn_OrderHistory'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.verifyElementVisible(findTestObject('TandC/link_ChangeLangEn'), FailureHandling.CONTINUE_ON_FAILURE)
+WebUI.waitForElementPresent(findTestObject('HomePage/label_ShipmentActivity'), 30)
 
-WebUI.verifyElementVisible(findTestObject('TandC/img_FetchrLogoAr'), FailureHandling.CONTINUE_ON_FAILURE)
+WebUI.verifyElementVisible(findTestObject('HomePage/label_ShipmentActivity'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.waitForElementClickable(findTestObject('TandC/link_ChangeLangEn'), 10, FailureHandling.STOP_ON_FAILURE)
+WebUI.waitForElementClickable(findTestObject('HomePage/btn_Exit'), 30)
 
-WebUI.click(findTestObject('TandC/link_ChangeLangEn'), FailureHandling.STOP_ON_FAILURE)
+WebUI.click(findTestObject('HomePage/btn_Exit'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.waitForElementPresent(findTestObject('TandC/link_ChangeLangAr'), 30, FailureHandling.STOP_ON_FAILURE)
+WebUI.waitForElementClickable(findTestObject('HomePage/btn_RescheduleOrder'), 30)
 
-WebUI.verifyElementVisible(findTestObject('TandC/link_ChangeLangAr'), FailureHandling.STOP_ON_FAILURE)
+WebUI.click(findTestObject('HomePage/btn_RescheduleOrder'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.verifyElementVisible(findTestObject('TandC/img_FetchrLogoEn'), FailureHandling.STOP_ON_FAILURE)
+WebUI.waitForElementPresent(findTestObject('Map/btn_ConfirmSchedule'), 30)
 
-WebUI.click(findTestObject('TandC/img_FetchrLogoEn'), FailureHandling.STOP_ON_FAILURE)
+WebUI.verifyElementVisible(findTestObject('Map/btn_ConfirmSchedule'), FailureHandling.STOP_ON_FAILURE)
 
-WebUI.waitForElementPresent(findTestObject('HomePage/btn_Search'), 30)
+WebUI.verifyElementVisible(findTestObject('Map/btn_CurrentLocation'), FailureHandling.STOP_ON_FAILURE)
 
-temp = WebUI.getUrl()
+WebUI.verifyElementVisible(findTestObject('Map/btn_FullScreen'), FailureHandling.STOP_ON_FAILURE)
 
-if (!(temp.equals('http://track.fetchr.us'))) {
-    assert false
-}
+WebUI.verifyElementVisible(findTestObject('Map/btn_Minus'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Map/btn_Plus'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Map/icon_CallUs'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Map/icon_Clear'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Map/img_FetchrLogoEn'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Map/label_CallUs'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Map/label_MovePin'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Map/link_ChangeLangAr'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Map/textBox_WhereToDeliver'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.setText(findTestObject('Map/textBox_WhereToDeliver'), GlobalVariable.Location)
+
+WebUI.sendKeys(findTestObject('Map/textBox_WhereToDeliver'), Keys.chord(Keys.BACK_SPACE, Keys.DOWN, Keys.ENTER))
+
+WebUI.verifyElementVisible(findTestObject('Map/label_Adress'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementText(findTestObject('Object Repository/Map/label_Adress'), GlobalVariable.Location)
+
+WebUI.waitForElementClickable(findTestObject('Map/btn_ConfirmSchedule'), 30)
+
+WebUI.click(findTestObject('Map/btn_ConfirmSchedule'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.waitForElementPresent(findTestObject('Delivery Location/label_DeliveryLocation'), 30)
+
+WebUI.verifyElementVisible(findTestObject('Delivery Location/label_DeliveryLocation'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Delivery Location/label_DeliveryLocationDesc'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementText(findTestObject('Delivery Location/label_DeliveryLocationDesc'), '11 5th Street')
+
+WebUI.verifyElementVisible(findTestObject('Delivery Location/label_HelpOurDrivers'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Delivery Location/textBox_driverCommentTxt'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Delivery Location/label_SelectDateTime'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Delivery Location/img_dateLeft'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Delivery Location/img_dateRight'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Delivery Location/label_Date'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Delivery Location/label_Time'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Delivery Location/btn_Back'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementVisible(findTestObject('Delivery Location/btn_ConfirmDetails'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.waitForElementClickable(findTestObject('Delivery Location/btn_ConfirmDetails'), 30)
+
+WebUI.click(findTestObject('Delivery Location/btn_ConfirmDetails'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.waitForElementClickable(findTestObject('Delivery Location/label_mshHeader'), 30)
+
+WebUI.verifyElementVisible(findTestObject('Delivery Location/label_mshHeader'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementText(findTestObject('Delivery Location/label_mshHeader'), 'Delivery Successfully Scheduled')
+
+WebUI.verifyElementVisible(findTestObject('Delivery Location/label_TNNumber'), FailureHandling.STOP_ON_FAILURE)
+
+WebUI.verifyElementText(findTestObject('Delivery Location/label_TNNumber'), GlobalVariable.TN5)
+
+WebUI.waitForElementClickable(findTestObject('Delivery Location/btn_BacktoHomepage'), 30)
+
+WebUI.click(findTestObject('Delivery Location/btn_BacktoHomepage'), FailureHandling.STOP_ON_FAILURE)
 
